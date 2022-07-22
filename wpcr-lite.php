868928114ad768138dbd05a022d612bd4c3b05f8
TI_Eugene <ti.eugene@gmail.com>
2022-07-23 01:53:15 +0300

    Code reformat

--- wpcr-lite.php
+++ wpcr-lite.php
@@ -9,38 +9,42 @@
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wpcr-lite
  *
- * Uses WPCRL_URL const
  * TODO: handle errors
  * TODO: plugin/theme
+ * WARNING: tune /etc/hosts @ customer vhost
  */
 
 class WPCRL_Updater {
-    private string $file;      // :str - abs path
-	private array $plugin;    // :array - ['Version'[, 'PluginURI', 'UpdateURI', ...]
-	private string $basename;  // :str - rel slug/slug.php
+	private string $file;       // :str - abs path
+	private array $plugin;      // :array - ['Version'[, 'PluginURI', 'UpdateURI', ...]
+	private string $basename;   // :str - rel slug/slug.php
 	private string $slug;
 	private string $version;
-	private bool $active;
-    public function __construct( $file ) {
-		error_log("WPCRL_Updater started for ".$file);
-        $this->file = $file;
-        add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );  // wp-admin
-        add_filter( 'pre_set_site_transient_update_plugins', array( &$this, 'check_update' ) );
-        add_filter( 'plugins_api', array( &$this, 'check_info' ), 10, 3 );
-	    return $this;
-    }
-	public function set_plugin_properties() : void {  // calling from wp-admin only
-		error_log("set_plugin_properties()");
-		$this->plugin  = get_plugin_data( $this->file );
-		$this->basename = plugin_basename( $this->file );
-		$this->slug = dirname($this->basename);  // or current(explode('/', $this->basename))
-		$this->version = $this->plugin['Version'];
-		$this->active = is_plugin_active( $this->basename );
+
+	//private bool $active;       // ? disable if not active ?
+	public function __construct( $file ) {
+		error_log( "WPCRL_Updater started for " . $file );
+		$this->file = $file;
+		add_action( 'admin_init', array( $this, 'set_plugin_properties' ) );  // wp-admin
+		add_filter( 'pre_set_site_transient_update_plugins', array( &$this, 'check_update' ) );
+		add_filter( 'plugins_api', array( &$this, 'check_info' ), 10, 3 );
+
+		return $this;
 	}
+
+	public function set_plugin_properties(): void {  // calling from wp-admin only
+		error_log( "set_plugin_properties()" );
+		$this->plugin   = get_plugin_data( $this->file );
+		$this->basename = plugin_basename( $this->file );
+		$this->slug     = dirname( $this->basename );  // or current(explode('/', $this->basename))
+		$this->version  = $this->plugin['Version'];
+		//$this->active = is_plugin_active( $this->basename );
+	}
+
 	public function check_update( $transient ) {
 		// slot #1: pre_set_site_transient_update_plugins()
 		// just checks whether plugin update available
-		error_log("check_update()");
+		error_log( "check_update()" );
 		if ( empty( $transient->checked ) ) {
 			return $transient;
 		}
@@ -49,51 +53,57 @@ class WPCRL_Updater {
 		$remote_meta = $this->get_remote_meta();
 
 		// If a newer version is available, add the update
-		error_log("Versions: this=".$this->version.", remote=".$remote_meta->version);
+		error_log( "Versions: this=" . $this->version . ", remote=" . $remote_meta->version );
 		if ( version_compare( $this->version, $remote_meta->version, '<' ) ) {
-			error_log("Need update");
-			$response = array(
+			error_log( "Need update" );
+			$response                               = array(
 				//'url' => $remote_meta->url,  // this->plugin["PluginURI"]
-				'slug' => $this->slug,  // short
-				'package' => $remote_meta->url,
+				'slug'        => $this->slug,  // short
+				'package'     => $remote_meta->url,
 				'new_version' => $remote_meta->version
 			);
-			$transient->response[$this->basename] = (object) $response;
+			$transient->response[ $this->basename ] = (object) $response;
 		}
+
 		return $transient;
 	}
-	public function check_info($obj, $action, $arg) {
+
+	public function check_info( $obj, $action, $arg ) {
 		// slot for plugins_api
-		error_log("check_info()");
-		error_log($action);
-		if (!empty( $args->slug ) && $arg->slug === $this->slug) {
+		error_log( "check_info()" );
+		error_log( $action );
+		if ( ! empty( $args->slug ) && $arg->slug === $this->slug ) {
 			$remote_meta = $this->get_remote_meta();
-			$response = array(
-				'slug' => $this->slug,
-				'version' => $remote_meta->version,  // or 'new_version' => ... ?
+			$response    = array(
+				'slug'          => $this->slug,
+				'version'       => $remote_meta->version,  // or 'new_version' => ... ?
 				'download_link' => $remote_meta->url,
-				'last_updated' => '2022-07-01',
-				'name' => $this->plugin["Name"],
-				'requires' => '4.0',
-				'tested' => '4.0',
-				'downloaded' => '16384',
-				'sections'			=> array(
-					'Description'	=> $this->plugin["Description"]
+				'last_updated'  => '2022-07-01',
+				'name'          => $this->plugin["Name"],
+				'requires'      => '4.0',
+				'tested'        => '4.0',
+				'downloaded'    => '16384',
+				'sections'      => array(
+					'Description' => $this->plugin["Description"]
 				)
 			);
+
 			return (object) $response;
 		}
+
 		return $obj;
 	}
-	private function get_remote_meta() : object|bool {
-		error_log("get_remote_meta()");
-		$request = wp_remote_get(WPCRL_URL.'plugins/'.$this->slug.'.json');
+
+	private function get_remote_meta(): object|bool {
+		error_log( "get_remote_meta()" );
+		$request = wp_remote_get( WPCRL_URL . 'plugins/' . $this->slug . '.json' );
 
 		// Check if response is valid
-		if ( !is_wp_error( $request ) || wp_remote_retrieve_response_code( $request ) === 200 ) {
+		if ( ! is_wp_error( $request ) || wp_remote_retrieve_response_code( $request ) === 200 ) {
 			//error_log(@json_encode($request));
-			return @json_decode($request['body']);
+			return @json_decode( $request['body'] );
 		}
+
 		return false;
 	}
 }
