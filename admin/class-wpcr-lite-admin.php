30f9bd9b69763be49898f20a1d77c729e27b0717
TI_Eugene <ti.eugene@gmail.com>
2022-07-24 10:14:39 +0300

    UI flush

--- admin/class-wpcr-lite-admin.php
+++ admin/class-wpcr-lite-admin.php
@@ -8,8 +8,8 @@ class WPCRL_Admin {
 	public function self_add_options_page() : void {
 		error_log("WPCRL_Admin.self_add_options_page()");
 		add_options_page(
-			'WPCRL admin page title',
-			'WPCRL admin menu title',
+			'WPCRL settings',  // sittings page <title>
+			'WPCRL',  // admin menu title (wp-admin -> settings -> ...)
 			'manage_options',
 			'wpcrl-settings',
 			array ($this, 'settings_page')
